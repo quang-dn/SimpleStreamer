f2189f7b71719dc66911a127c422d8d2546e2787
ThienPhong <thienphong.wind@gmail.com>
2014-05-12 13:12:29 +1000

    Updated ServerThread

--- src/simple_streamer/ServerThread.java
+++ src/simple_streamer/ServerThread.java
@@ -1,5 +1,43 @@
 package simple_streamer;
 
-public class ServerThread {
+import java.io.*;
+import java.net.*;
 
+public class ServerThread implements Runnable {
+
+	/*
+	 * Main logic
+	 */
+	@Override
+	public void run() {
+		int port = 6262;
+		ServerSocket serverSocket;
+		try {
+			while (true) {
+				serverSocket = new ServerSocket(port);
+				System.out.println("Waiting for client on port "
+						+ serverSocket.getLocalPort() + "...");
+				Socket server = serverSocket.accept();
+				System.out.println("Connected to "
+						+ server.getRemoteSocketAddress());
+
+				// Send JSON features message
+				PrintWriter toClient = new PrintWriter(
+						server.getOutputStream(), true);
+				String JSONMsg = "";
+				toClient.print(JSONMsg);
+
+				// Receive client requests
+				BufferedReader fromClient = new BufferedReader(
+						new InputStreamReader(server.getInputStream()));
+				String clientMsg = fromClient.readLine();
+			}
+		} catch (UnknownHostException ex) {
+			ex.printStackTrace();
+			System.exit(-1);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+	}
 }
