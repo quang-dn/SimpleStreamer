17261f1e7eea8a57882a8545173af27a97358552
ThienPhong <thienphong.wind@gmail.com>
2014-05-12 13:30:19 +1000

    Change LocalView to EntryPoint

--- src/simple_streamer/EntryPoint.java
+++ src/simple_streamer/EntryPoint.java
@@ -19,7 +19,7 @@ import com.github.sarxos.webcam.ds.buildin.natives.Device;
 import com.github.sarxos.webcam.ds.buildin.natives.DeviceList;
 import com.github.sarxos.webcam.ds.buildin.natives.OpenIMAJGrabber;
 
-public class LocalView {
+public class EntryPoint {
 	
 	public static void main(String[] args) {
 		
